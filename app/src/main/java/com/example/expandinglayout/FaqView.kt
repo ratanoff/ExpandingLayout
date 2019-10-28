74bd85a7ea694195b56a3ab27f674f8cff5fc7e8
Leonid Ratanov <leonid.ratanoff@gmail.com>
2019-10-28 18:44:16 +0500

    tec

--- app/src/main/java/com/example/expandinglayout/FaqView.kt
+++ app/src/main/java/com/example/expandinglayout/FaqView.kt
@@ -1,4 +1,30 @@
 package com.example.expandinglayout
 
-class FaqView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.view_faq.view.*
+
+class FaqView(context: Context, attrs: AttributeSet) : MotionLayout(context, attrs) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_faq, this, true)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.FaqView,
+            0, 0
+        ).apply {
+            try {
+                title.text = getString(R.styleable.FaqView_title)
+                description.text = getString(R.styleable.FaqView_description)
+            } finally {
+                recycle()
+            }
+        }
+
+
+    }
+
 }
